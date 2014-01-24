284f1669b874fbb71e17876fffcf022a12deae1e
Jayme <kalkehcoisa@gmail.com>
2014-01-24 13:10:27 -0200

    Sending the news configs.

--- pycicu/__init__.py
+++ pycicu/__init__.py
@@ -3,10 +3,9 @@
 from .widget import (CropedPhotoUploaderInput, deferred_croped_picture, 
                      pyCreateResourceRegistry)
 
-# Number of seconds to keep uploaded files. The clean_uploaded command will
-# delete them after this has expired.
-UPLOADER_DELETE_AFTER =  60 * 60
-IMAGE_CROPPED_UPLOAD_TO = 'pycicu_cropped/'
+from sqlalchemy import engine_from_config
+
+from .models import (DBSession, Base )
 
 def routes(config):
     config.add_static_view('static', 'pycicu:static', cache_max_age=0)
@@ -15,6 +14,12 @@ def routes(config):
     config.add_route('pycicu-upload', '/ajax-upload/')
 
 def includeme(config):
+    settings = config.registry.settings
+    
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+    
     config.include(routes, route_prefix='/pycicu/')
     config.scan()
     
