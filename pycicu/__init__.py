53146dcb577e93834c970f84b1d6e07ba3689bcf
Jayme <kalkehcoisa@gmail.com>
2014-01-29 08:58:17 -0200

    Adjusted the cache duration and the uid generation for the uploaded objects.

--- pycicu/__init__.py
+++ pycicu/__init__.py
@@ -8,24 +8,16 @@ from sqlalchemy import engine_from_config
 from .models import (DBSession, Base)
 
 def routes(config):
-    config.add_static_view('static', 'pycicu:static', cache_max_age=0)
+    config.add_static_view('static', 'pycicu:static', cache_max_age=3600)
     
     config.add_route('pycicu-crop', '/ajax-upload/crop/')
     config.add_route('pycicu-upload', '/ajax-upload/')
 
 def includeme(config):
     settings = config.registry.settings
-    
-    #global DBSession
-    #global Base
-    
-    #if 'DBSession' not in settings:
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
-    #else:
-    #    DBSession = settings['DBSession']
-    #    Base = settings['Base']
     
     config.include(routes, route_prefix='/pycicu/')
     config.scan()
